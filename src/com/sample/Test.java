56d8c65f99a4184e33d1e03e91d27febc103e17e
Sudarshan Surnar <sudrshans25@gmail.com>
2024-01-21 16:29:42 +0530

    add code

--- src/com/sample/Test.java
+++ src/com/sample/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hellw Word");
-
+		System.out.println("11111111111111111");
+		System.out.println("22222222222222222");
 	}
 
 }
